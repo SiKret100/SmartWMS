f0f8c3c417fc01ba2da3378fbcec94fe1eb9250e
SaWyyy <kamilsadowski001@gmail.com>
2024-09-19 19:07:16 +0200

    Add fields to alert dto
    Co-authored-by: r00j3K <r00j3K@users.noreply.github.com>

--- data/DTOs/alertDto.js
+++ data/DTOs/alertDto.js
@@ -1,10 +1,14 @@
 export default class AlertDto{
     seen;
+    title;
+    description;
     alertDate;
     alertType;
 
     constructor(data){
         this.seen = false;
+        this.title = data.title;
+        this.description = data.description;
         this.alertDate = data.alertDate;
         this.alertType = data.alertType;
     }
