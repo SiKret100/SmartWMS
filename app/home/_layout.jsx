999a2257cd4cbc7a9a41e1bb37babd5f3ab43ac9
SaWyyy <kamilsadowski001@gmail.com>
2024-09-27 19:05:31 +0200

    Add temporary role-based navigation
    Co-authored-by: r00j3K <r00j3K@users.noreply.github.com>

--- app/home/_layout.jsx
+++ app/home/_layout.jsx
@@ -1,100 +1,124 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Drawer from "expo-router/drawer";
+import { createContext } from 'react';
+import { useEffect, useState, useContext } from "react";
+import authService from '../../services/auth/authService';
+
+export const UserDataContext = createContext({});
 
 const RootLayout = () => {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try{
+        const res = await authService.getUserInfo();
+        setUserData(res.data);
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+    fetchUserInfo();
+  }, [])
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
-        <Drawer.Screen 
-          name='index' 
-          options={{
-            drawerLabel: 'Strona główna',
-            title: 'Strona główna',
-          }}
-        />
-        
-        <Drawer.Screen 
-          name='alerts' 
-          options={{
-            drawerLabel: 'Alerty',
-            title: 'Alerty',
-          }}
-        />
+    
+        <GestureHandlerRootView style={{ flex: 1 }}>
+      <UserDataContext.Provider value={userData}>
+        <Drawer>
+          <Drawer.Screen 
+            name='index' 
+            options={{
+              drawerLabel: 'Strona główna',
+              title: 'Strona główna',
+            }}
+          />
 
-        <Drawer.Screen 
-          name='categories' 
-          options={{
-            drawerLabel: 'Kategorie',
-            title: 'Kategorie',
-          }}
-        />
+          <Drawer.Screen 
+            name='alerts'
+            redirect={userData.role == 'Employee'}
+            options={{
+              drawerLabel: 'Alerty',
+              title: 'Alerty',
+            }}
+          />
 
-        <Drawer.Screen 
-          name='orders' 
-          options={{
-            drawerLabel: 'Zamówienia',
-            title: 'Zamówienia',
-          }}
-        />
+          <Drawer.Screen 
+            name='categories' 
+            options={{
+              drawerLabel: 'Kategorie',
+              title: 'Kategorie',
+            }}
+          />
 
-        <Drawer.Screen 
-          name='products' 
-          options={{
-            drawerLabel: 'Produkty',
-            title: 'Produkty',
-          }}
-        />
+          <Drawer.Screen 
+            name='orders' 
+            options={{
+              drawerLabel: 'Zamówienia',
+              title: 'Zamówienia',
+            }}
+          />
 
-        <Drawer.Screen 
-          name='reports' 
-          options={{
-            drawerLabel: 'Raporty',
-            title: 'Raporty',
-          }}
-        />
-        
-        <Drawer.Screen 
-          name='shelves' 
-          options={{
-            drawerLabel: 'Półki',
-            title: 'Półki',
-          }}
-        />
+          <Drawer.Screen 
+            name='products' 
+            options={{
+              drawerLabel: 'Produkty',
+              title: 'Produkty',
+            }}
+          />
 
-        <Drawer.Screen 
-          name='subcategories' 
-          options={{
-            drawerLabel: 'Podkategorie',
-            title: 'Podkategorie',
-          }}
-        />
+          <Drawer.Screen 
+            name='reports' 
+            options={{
+              drawerLabel: 'Raporty',
+              title: 'Raporty',
+            }}
+          />
+          
+          <Drawer.Screen 
+            name='shelves' 
+            options={{
+              drawerLabel: 'Półki',
+              title: 'Półki',
+            }}
+          />
 
-        <Drawer.Screen 
-          name='tasks' 
-          options={{
-            drawerLabel: 'Zadania',
-            title: 'Zadania',
-          }}
-        />
+          <Drawer.Screen 
+            name='subcategories' 
+            options={{
+              drawerLabel: 'Podkategorie',
+              title: 'Podkategorie',
+            }}
+          />
 
-        <Drawer.Screen 
-          name='users' 
-          options={{
-            drawerLabel: 'Użytkownicy',
-            title: 'Użytkownicy',
-          }}
-        />
+          <Drawer.Screen 
+            name='tasks' 
+            options={{
+              drawerLabel: 'Zadania',
+              title: 'Zadania',
+            }}
+          />
 
-        <Drawer.Screen 
-          name='waybills' 
-          options={{
-            drawerLabel: 'Listy przewozowe',
-            title: 'Listy przewozowe',
-          }}
-        />
-      </Drawer>
+          <Drawer.Screen 
+            name='users' 
+            options={{
+              drawerLabel: 'Użytkownicy',
+              title: 'Użytkownicy',
+            }}
+          />
+
+          <Drawer.Screen 
+            name='waybills' 
+            options={{
+              drawerLabel: 'Listy przewozowe',
+              title: 'Listy przewozowe',
+            }}
+          />
+        </Drawer>
+      </UserDataContext.Provider>
     </GestureHandlerRootView>
-  );
+      )
 };
 
 export default RootLayout;
--- app/home/index.jsx
+++ app/home/index.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState, useContext } from 'react';
 import { Text, View } from 'react-native'
+import axios from "axios";
+import authService from '../../services/auth/authService';
+import { UserDataContext } from './_layout.jsx';
+
+
 
 const HomePage = () => {
+  const userData = useContext(UserDataContext);
+  const [error, setError] = useState("");
+
   return (
     <View>
-      <Text>Strona główna</Text>
+        <Text> {userData.role} </Text> 
     </View>
   )
 }
