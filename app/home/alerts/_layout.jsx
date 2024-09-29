c2328276347437c2f3b2dd11ac28a629088ceb15
SaWyyy <kamilsadowski001@gmail.com>
2024-09-29 18:57:08 +0200

    Customize tabs in alerts layout
    Co-authored-by: r00j3K <r00j3K@users.noreply.github.com>

--- app/home/alerts/_layout.jsx
+++ app/home/alerts/_layout.jsx
@@ -5,35 +5,40 @@ import Feather from "react-native-vector-icons/Feather";
 
 const AlertLayout = () => {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs screenOptions={
+      {
+        tabBarActiveTintColor: '#3E86D8',
+        tabBarStyle: {
+          height: 85
+        },
+        tabBarLabelStyle: {
+          fontSize: 13,
+          fontWeight: 'bold'
+        }
+      }
+    }>
       <Tabs.Screen name='index' options={{
-        title:"Alerty",
+        title: "Alerty",
         headerShown: false,
         tabBarIcon: ({ color, size }) => (
-          <Feather name="home" size={24} color="black" />
+          <Feather name="home" size={24} color={color} />
         )
-      }}/>
+      }} />
       <Tabs.Screen name='add' options={{
-        title:"Dodaj",
+        title: "Dodaj",
         headerShown: false,
         tabBarIcon: ({ color, size }) => (
-          <Feather name="plus-circle" size={24} color="black" />
+          <Feather name="plus-circle" size={24} color={color} />
         )
-      }}/>
-      {/* <Tabs.Screen name='update' options={{
-        title:"Edytuj",
+      }} />
+      <Tabs.Screen name='update' options={{
+        title: "Edytuj",
         headerShown: false,
         tabBarIcon: ({ color, size }) => (
           <Feather name="edit" size={24} color="black" />
-        )
-      }}/>
-      <Tabs.Screen name='delete' options={{
-        title:"Usuń",
-        headerShown: false,
-        tabBarIcon: ({ color, size }) => (
-          <Feather name="trash" size={24} color="black" />
-        )
-      }}/> */}
+        ),
+        tabBarButton: () => null
+      }} />
     </Tabs>
   )
 }
