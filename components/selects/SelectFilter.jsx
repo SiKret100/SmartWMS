6ca61b1f4fc24c8edf049174b86e0de5abdea45d
SaWyyy <kamilsadowski001@gmail.com>
2024-10-02 18:57:09 +0200

    Add template for select list filter as separate component
    Co-authored-by: r00j3K <r00j3K@users.noreply.github.com>

---
+++ components/selects/SelectFilter.jsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { SelectList } from 'react-native-dropdown-select-list'
+
+
+const CustomSelectList = ({selectKey, setForm, alertTypeMap, form}) => {
+
+  const defaultOption = form.alertType !== -1 ? alertTypeMap.find(item => item.key === form.alertType) : null;
+
+  return (
+    <SelectList
+      key={selectKey}
+      setSelected={(val) => setForm((prevForm) => ({ ...prevForm, alertType: val }))}
+      data={alertTypeMap}
+      save="key" 
+      placeholder="Select type..."
+      boxStyles={{
+        borderColor: 'black',
+        borderWidth: 2,
+        height: 56,
+        borderRadius: 13,
+        alignItems: 'center'
+      }}
+      inputStyles={{ fontSize: 16 }}
+      dropdownTextStyles={{ fontSize: 16 }}
+      defaultOption = {defaultOption}
+    />
+  )
+}
+
+export default CustomSelectList
+
+const styles = StyleSheet.create({})
